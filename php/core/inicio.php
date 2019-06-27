47723efc1234939c79b1c44a91737c48bf9d044c
fiorelaalcaraz <fiorellalcaraz@gmail.com>
2019-06-26 20:06:56 -0400

    Update inicio.php

--- php/core/inicio.php
+++ php/core/inicio.php
@@ -45,7 +45,7 @@ include "menubar.php";
         <!--Fin Titulo Contenido -->
         <!--Inicio de la pagina contenido -->
         <div class="row-fluid">
-            <img src="http://thttp://betscore.herokuapp.com/source/img/banner_futbol.jpg" height="276">
+            <img src="http://betscore.herokuapp.com/source/img/banner_futbol.jpg" height="276">
         </div>
         <div class="row-fluid" style="color: #ffffff;">
             <div class="row-fluid box-row">
