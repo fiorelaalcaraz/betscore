8088321d4bb319992e0df825b445946204e8b250
IVANS\Ivan Samudio <fiorellalcaraz@gmail.com>
2019-07-28 16:49:12 -0400

    Update api_connect.php

--- php/class/api_connect.php
+++ php/class/api_connect.php
@@ -46,7 +46,23 @@ foreach($array_par as $row){
 
   $data = '{'.$id_fixture.'}'.'{'.$round.'}'.'{'.$id_equipo_local.'}'.'{'.$id_equipo_visi.'}'.'{'.$goles_local.'}'.'{'.$goles_visi.'}'.'{'.$fec_partido.'}'.'<br/>';
 echo ($data);
-$sql = pg_query("insert into fixtures values ($id_fixture,'',$id_equipo_local,$id_equipo_visi,'1',$goles_local,$goles_visi,'$fec',$jor,'$hor'); ");
+
+if (!isset($goles_local)){
+
+	
+	$sql = pg_query("update fixtures set goles_local = null, goles_visi = null where id_partido = $id_fixture ;");
+	$noticia = pg_last_notice($con->url);
+	echo str_replace("NOTICE: ","",$noticia);
+  	
+}else{
+  
+  $sql = pg_query("update fixtures set goles_local = $goles_local, goles_visi = $goles_visi where id_partido = $id_fixture ;"); 	
+	
+	$noticia = pg_last_notice($con->url);
+	echo str_replace("NOTICE: ","",$noticia);
+  };
+  
+//$sql = pg_query("insert into fixtures values ($id_fixture,'',$id_equipo_local,$id_equipo_visi,'1',$goles_local,$goles_visi,'$fec',$jor,'$hor'); ");
 $noticia = pg_last_notice($con->url);
 echo str_replace("NOTICE: ","",$noticia);
 
