3b7718b67b7d900570ebfb98b2dadd5301053134
IVANS\Ivan Samudio <fiorellalcaraz@gmail.com>
2020-01-13 19:06:15 -0300

    Update registro.php

--- php/class/registro.php
+++ php/class/registro.php
@@ -58,7 +58,7 @@ require 'sesion.php';
             llevame_a("../../index.php");
         }
    }else{
-       msg_sesion("$noticia");
+       msg_sesion(str_replace("NOTICE: ","",$noticia));
        llevame_a("../core/reg_usu/reg_usu.php");
    }
  ?>
