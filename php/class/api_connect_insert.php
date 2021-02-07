9bd02b7c49d05e8e1c7c848908fd86dce9ab7636
IVANS\Ivan Samudio <fiorellalcaraz@gmail.com>
2021-02-07 15:13:57 -0300

    Update api_connect_insert.php

--- php/class/api_connect_insert.php
+++ php/class/api_connect_insert.php
@@ -65,7 +65,7 @@ if (!isset($goles_local)){
 $sql = pg_query("insert into fixtures values ($id_fixture,'',$id_equipo_local,$id_equipo_visi,'3',$goles_local,$goles_visi,'$fec',$jor,'$hor'); ");
 $noticia = pg_last_notice($con->url);
 echo str_replace("NOTICE: ","",$noticia);
-echo ($jor);
+echo ($id_fixture.$jor);
 //("insert into fixtures values //($id_fixture,'',$id_equipo_local,$id_equipo_visi,'3',$goles_local,$goles_visi,'$fec',$jor,'$hor'); ");
 }
 
