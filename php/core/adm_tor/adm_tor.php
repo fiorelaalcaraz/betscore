8f56d4845d50e9b60b9d8ed0f458306a051ab6e4
IVANS\Ivan Samudio <fiorellalcaraz@gmail.com>
2021-02-07 18:42:27 -0300

    Update adm_tor.php

--- php/core/adm_tor/adm_tor.php
+++ php/core/adm_tor/adm_tor.php
@@ -1,8 +1,8 @@
 <?php 
-require "../../config/funciones.php";
-require "../../class/sesion.php";
-require "../../class/torneo.php";
-require "../../class/liga.php";
+require "../../php/config/funciones.php";
+require "../../php/class/sesion.php";
+require "../../php/class/torneo.php";
+require "../../php/class/liga.php";
 
 
 //verifica session
