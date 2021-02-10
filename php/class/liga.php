98f263db16d6c6e79b88d1b86a3afbcf26b07e7e
IVANS\Ivan Samudio <fiorellalcaraz@gmail.com>
2021-02-10 19:59:14 -0300

    Update liga.php

--- php/class/liga.php
+++ php/class/liga.php
@@ -20,6 +20,7 @@ class Liga{
      
     // obtener ligas
     public function get_ligas(){
+        echo "<option value='".$lig["id_liga"]."'>".$lig["descrip_liga"]."</option>";
         $con = new conexion();
         $con ->conectar();
         $liga = pg_query("select * from ligas order by 1;");
